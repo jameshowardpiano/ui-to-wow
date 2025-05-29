35fba03307d1ac63a955c5329f92478a430b52de
James Flood Jr <floodadosious111@gmail.com>
2025-05-29 09:12:11 -0400

    render change

--- server.cjs
+++ server.cjs
@@ -26,6 +26,10 @@ app.get("/api/search", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("API is running.");
+});
+
 app.listen(process.env.PORT || 3001, () => {
   console.log("Proxy server running on port 3001")
 });
